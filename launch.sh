53d4d1bc03f9ef358e4c85250b06d8f0ca20e1b2
Kurt Lieber <kurt@lieber.org>
2023-09-19 17:43:59 -0700

    fixed typos and shit

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if ! pgrep -x "i3wdp.py" > /dev/null
+if ! pgrep -f "i3wdp.py" > /dev/null
 then
-   $HOME/source/i3-wpd/ipwdp.py $HOME/source/i3-wpd/16-10/ .jpg 
+   $HOME/source/i3-wpd/i3wpd.py $HOME/source/i3-wpd/16-10/ .jpg &
 fi
 
